8e20e20abe493d01acc1f4356becf9d86d6a92cd
Evelyn Barrios <evy2bar@gmail.com>
2015-12-07 12:26:56 -0500

    added Route Boxer to diferents routes

--- finalProject/funTrip/app/assets/javascripts/application.js
+++ finalProject/funTrip/app/assets/javascripts/application.js
@@ -13,5 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
+//= require route_boxer
 //= require trips
 
--- finalProject/funTrip/db/schema.rb
+++ finalProject/funTrip/db/schema.rb
@@ -17,6 +17,7 @@ ActiveRecord::Schema.define(version: 20151126152121) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
+    t.string   "places"
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
