314285939569c7077662d4212af8bf917147fd70
Evelyn Barrios <evy2bar@gmail.com>
2015-12-10 12:36:24 -0500

    added direction panel

--- finalProject/funTrip/app/assets/javascripts/direction_panel.js
+++ finalProject/funTrip/app/assets/javascripts/direction_panel.js
@@ -1,6 +1,33 @@
-/**
- * Created by Eve on 12/9/15.
- */
-function callbackSwowPanel(data, status, response) {
-    console.log('this is my amazin panel - ' + data)
+//function CenterControl(map) {
+//    var controlDiv = document.createElement('div');
+//    var controlUI = document.createElement('div');
+//    controlDiv.appendChild(controlUI);
+//
+//    var controlText = document.createElement('div');
+//    controlText.innerHTML = 'PANEL';
+//    controlUI.appendChild(controlText);
+//
+//    controlUI.addEventListener('click', function() {
+//        map.setCenter(from);
+//    });
+//
+//    $(controlDiv).addClass("center-control")
+//
+//    this.element = controlDiv;
+//}
+
+
+
+function getDirectionPanelHtml () {
+    var directionPanel = $(".direction-panel");
+
+    // Get DOM element from jQuery because that's what Google Maps understands
+    return directionPanel[0];
 }
+
+
+$(document).on("ready", function () {
+    $(".js-hola").on("click", function () {
+        alert("click en hola");
+    });
+})
--- finalProject/funTrip/app/assets/javascripts/maps.js
+++ finalProject/funTrip/app/assets/javascripts/maps.js
@@ -2,6 +2,8 @@ function repeatingRequest(request) {
     function callback (results, status) {
         if (status == google.maps.places.PlacesServiceStatus.OK) {
             showMarkers(results);
+
+
         }
         else if (status == google.maps.places.PlacesServiceStatus.OVER_QUERY_LIMIT) {
             setTimeout(makeMapsRequest, 500);
